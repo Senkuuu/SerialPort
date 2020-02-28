2ad860170899e82030c497f3ad1b00334f860421
cshuangfei <44801621+cshuangfei@users.noreply.github.com>
2020-02-28 14:33:45 +0800

    新增物料绑定信息、Redis版本更换

--- Accenture.SerialPort/DataToRedis.cs
+++ Accenture.SerialPort/DataToRedis.cs
@@ -16,28 +16,20 @@ namespace Accenture.SerialPort
 {
     public class RedisHelper
     {
-
-        //获取Redis服务器地址
-        public static string path = ConfigurationManager.AppSettings["RedisPath"];
-        public static string Port = ConfigurationManager.AppSettings["Port"];
-        public static string Password = ConfigurationManager.AppSettings["Password"];
-        //连接Redis服务器,path:服务器地址，Port:端口，Password：密码，访问的数据库
-        public RedisClient Redis = new RedisClient(path, int.Parse(Port), Password, 0);
-        //缓存池
-        PooledRedisClientManager prcm = new PooledRedisClientManager();
-
         /// <summary>
         /// Redis缓存
         /// </summary>
         /// <param name="dtb"></param>
         /// <returns></returns>
-        public bool DtToRedis(DataTable dtb, string type)
+        public bool DtToRedis(DataTable dtb, string type, RedisClient Redis)
         {
             string key = "";
             if (type.Equals("WMS_PB_Equipment"))
                 key = "SN";
             else if (type.Equals("WMS_BT_AlarmRule"))
                 key = "NO";
+            else if (type.Equals("WMS_BT_EquipmentBind"))
+                key = "EquipmentSNs";
             foreach (DataRow dr in dtb.Rows)
             {
                 Dictionary<string, object> drow = new Dictionary<string, object>();
@@ -45,11 +37,11 @@ namespace Accenture.SerialPort
                 {
                     drow.Add(dc.ColumnName, dr[dc.ColumnName]);
                 }
-                //WMS_PB_Equipment value = null;
+                WMS_BT_EquipmentBind value = null;
                 try
                 {
                     Redis.Set(dr[""+key+""].ToString(), drow);
-                    //value = Redis.Get<WMS_PB_Equipment>("key");//读取Redis
+                    value = Redis.Get<WMS_BT_EquipmentBind>(dr["" + key + ""].ToString());//读取Redis
                 }
                 catch (Exception e)
                 {
